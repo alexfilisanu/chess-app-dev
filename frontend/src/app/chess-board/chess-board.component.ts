dd132fcbc3a73c629fb4b5cf37cedc47cc7b5cc5
Vivivi <vivianapantazica@yahoo.ro>
2023-04-27 17:03:46 +0300

    add all pieces for white player
    Signed-off-by: Vivivi <vivianapantazica@yahoo.ro>

--- frontend/src/app/chess-board/chess-board.component.ts
+++ frontend/src/app/chess-board/chess-board.component.ts
@@ -11,12 +11,22 @@ import { GameService } from '../game-service/game.service';
 export class ChessBoardComponent {
     sixtyFour = new Array(64).fill(0).map((_, i) => i);
 
-    knightPosition$ = this.game.knightPosition$;
-    pawnPosition$ = this.game.pawnPosition$;
-    rookPosition$ = this.game.rookPosition$;
-    bishopPosition$ = this.game.bishopPosition$;
+    rookPosition1$ = this.game.rookPosition1$;
+    knightPosition1$ = this.game.knightPosition1$;
+    bishopPosition1$ = this.game.bishopPosition1$;
     queenPosition$ = this.game.queenPosition$;
     kingPosition$ = this.game.kingPosition$;
+    bishopPosition2$ = this.game.bishopPosition2$;
+    knightPosition2$ = this.game.knightPosition2$;
+    rookPosition2$ = this.game.rookPosition2$;
+    pawnPosition1$ = this.game.pawnPosition1$;
+    pawnPosition2$ = this.game.pawnPosition2$;
+    pawnPosition3$ = this.game.pawnPosition3$;
+    pawnPosition4$ = this.game.pawnPosition4$;
+    pawnPosition5$ = this.game.pawnPosition5$;
+    pawnPosition6$ = this.game.pawnPosition6$;
+    pawnPosition7$ = this.game.pawnPosition7$;
+    pawnPosition8$ = this.game.pawnPosition8$;
 
     selectedPosition: Coord | undefined;
 
@@ -26,78 +36,77 @@ export class ChessBoardComponent {
     }
 
     xy(i: number): Coord {
-          return {
-              x: i % 8,
-              y: Math.floor(i / 8)
-        }
+      return {
+        x: i % 8,
+        y: Math.floor(i / 8)
+      }
     }
 
     isBlack({ x, y }: Coord) {
-            return (x + y) % 2 === 1;
+      return (x + y) % 2 === 1;
     }
 
-      isSquareSelected(pos: Coord) {
-        return (
-          this.selectedPosition &&
-          this.selectedPosition.x === pos.x &&
-          this.selectedPosition.y === pos.y
-        );
+    isSquareSelected(pos: Coord) {
+      return (
+        this.selectedPosition &&
+        this.selectedPosition.x === pos.x &&
+        this.selectedPosition.y === pos.y
+      );
+    }
+
+    isValidMove(pos: Coord): boolean {
+      if (this.selectedPosition) {
+        const pieceType = this.game.getPieceType(this.selectedPosition);
+        console.log(pieceType)
+        this.validMoves = this.game.getValidMoves(pieceType, this.selectedPosition);
+        console.log(this.validMoves)
       }
-
-      isValidMove(pos: Coord): boolean {
-          // check if validMoves is defined and check if pos is in validMoves
-          if (this.selectedPosition) {
-            const pieceType = this.game.getPieceType(this.selectedPosition);
-            console.log(pieceType)
-            this.validMoves = this.game.getValidMoves(pieceType, this.selectedPosition);
-            console.log(this.validMoves)
-          }
-          return this.validMoves && this.validMoves.some(vm => vm.x === pos.x && vm.y === pos.y);
-      }
+      return this.validMoves && this.validMoves.some(vm => vm.x === pos.x && vm.y === pos.y);
+    }
 
 
-      handleSquareClick(pos: Coord) {
-        if (this.selectedPosition) {
-          const pieceType = this.game.getPieceType(this.selectedPosition);
-          if (!this.game.isPieceAt(pos)) {
-            switch(pieceType) {
-              case "knight":
-                if (this.game.canMoveKnight(this.selectedPosition, pos)) {
-                  this.game.moveKnight(this.selectedPosition, pos);
-                }
-                break;
-              case "pawn":
-                if (this.game.canMovePawn(this.selectedPosition, pos)) {
-                  this.game.movePawn(this.selectedPosition, pos);
-                }
-                break;
-              case "rook":
-                if (this.game.canMoveRook(this.selectedPosition, pos)) {
-                  this.game.moveRook(this.selectedPosition, pos);
-                }
-                break;
-              case "bishop":
-                if (this.game.canMoveBishop(this.selectedPosition, pos)) {
-                  this.game.moveBishop(this.selectedPosition, pos);
-                }
-                break;
-              case "queen":
-                if (this.game.canMoveQueen(this.selectedPosition, pos)) {
-                  this.game.moveQueen(this.selectedPosition, pos);
-                }
-                break;
-              case "king":
-                if (this.game.canMoveKing(this.selectedPosition, pos)) {
-                  this.game.moveKing(this.selectedPosition, pos);
-                }
-                break;
+    handleSquareClick(pos: Coord) {
+      if (this.selectedPosition) {
+        const pieceType = this.game.getPieceType(this.selectedPosition).piece;
+        const index = this.game.getPieceType(this.selectedPosition).index;
+        if (!this.game.isPieceAt(pos)) {
+          switch(pieceType) {
+          case "knight":
+            if (this.game.canMoveKnight(index, this.selectedPosition, pos)) {
+              this.game.moveKnight(index, this.selectedPosition, pos);
             }
-            this.validMoves = [];
-            this.selectedPosition = undefined;
+            break;
+          case "pawn":
+            if (this.game.canMovePawn(index, this.selectedPosition, pos)) {
+              this.game.movePawn(index, this.selectedPosition, pos);
+            }
+            break;
+          case "rook":
+            if (this.game.canMoveRook(index, this.selectedPosition, pos)) {
+              this.game.moveRook(index, this.selectedPosition, pos);
+            }
+            break;
+          case "bishop":
+            if (this.game.canMoveBishop(index, this.selectedPosition, pos)) {
+              this.game.moveBishop(index, this.selectedPosition, pos);
+            }
+            break;
+          case "queen":
+            if (this.game.canMoveQueen(index, this.selectedPosition, pos)) {
+              this.game.moveQueen(index, this.selectedPosition, pos);
+            }
+            break;
+          case "king":
+            if (this.game.canMoveKing(index, this.selectedPosition, pos)) {
+              this.game.moveKing(index, this.selectedPosition, pos);
+            }
+            break;
           }
-        } else {
-
-          this.selectedPosition = pos;
+          this.validMoves = [];
+          this.selectedPosition = undefined;
         }
+      } else {
+      this.selectedPosition = pos;
       }
+    }
 }
--- frontend/src/app/game-service/game.service.ts
+++ frontend/src/app/game-service/game.service.ts
@@ -7,34 +7,62 @@ import { Coord } from '../chess-board/coord';
 })
 export class GameService {
 
-      rookPosition$ = new BehaviorSubject<Coord>({ x: 0, y: 0 });
-      knightPosition$ = new BehaviorSubject<Coord>({ x: 1, y: 0 });
-      bishopPosition$ = new BehaviorSubject<Coord>({ x: 2, y: 0 });
+      rookPosition1$ = new BehaviorSubject<Coord>({ x: 0, y: 0 });
+      knightPosition1$ = new BehaviorSubject<Coord>({ x: 1, y: 0 });
+      bishopPosition1$ = new BehaviorSubject<Coord>({ x: 2, y: 0 });
       queenPosition$ = new BehaviorSubject<Coord>({ x: 3, y: 0 });
       kingPosition$ = new BehaviorSubject<Coord>({ x: 4, y: 0 });
-      pawnPosition$ = new BehaviorSubject<Coord>({ x: 0, y: 1 });
-      currentPosition: { knight: Coord, pawn: Coord, rook: Coord, bishop: Coord, queen: Coord, king: Coord } =
+      bishopPosition2$ = new BehaviorSubject<Coord>({ x: 5, y: 0 });
+      knightPosition2$ = new BehaviorSubject<Coord>({ x: 6, y: 0 });
+      rookPosition2$ = new BehaviorSubject<Coord>({ x: 7, y: 0 });
+      pawnPosition1$ = new BehaviorSubject<Coord>({ x: 0, y: 1 });
+      pawnPosition2$ = new BehaviorSubject<Coord>({ x: 1, y: 1 });
+      pawnPosition3$ = new BehaviorSubject<Coord>({ x: 2, y: 1 });
+      pawnPosition4$ = new BehaviorSubject<Coord>({ x: 3, y: 1 });
+      pawnPosition5$ = new BehaviorSubject<Coord>({ x: 4, y: 1 });
+      pawnPosition6$ = new BehaviorSubject<Coord>({ x: 5, y: 1 });
+      pawnPosition7$ = new BehaviorSubject<Coord>({ x: 6, y: 1 });
+      pawnPosition8$ = new BehaviorSubject<Coord>({ x: 7, y: 1 });
+
+      currentPosition: { rook1: Coord, knight1: Coord, bishop1: Coord , queen: Coord, king: Coord, bishop2: Coord, knight2: Coord, rook2: Coord,
+                         pawn1: Coord, pawn2: Coord, pawn3: Coord, pawn4: Coord, pawn5: Coord, pawn6: Coord, pawn7: Coord, pawn8: Coord } =
       {
-        knight: { x: 1, y: 0 },
-        pawn: { x: 0, y: 1 },
-        rook: { x: 0, y: 0 },
-        bishop: { x: 2, y: 0 },
+        rook1: { x: 0, y: 0 },
+        knight1: { x: 1, y: 0 },
+        bishop1: { x: 2, y: 0 },
         queen: { x: 3, y: 0 },
-        king: { x: 4, y: 0 }
+        king: { x: 4, y: 0 },
+        bishop2: { x: 5, y: 0 },
+        knight2: { x: 6, y: 0 },
+        rook2: { x: 7, y: 0 },
+        pawn1: { x: 0, y: 1 },
+        pawn2: { x: 1, y: 1 },
+        pawn3: { x: 2, y: 1 },
+        pawn4: { x: 3, y: 1 },
+        pawn5: { x: 4, y: 1 },
+        pawn6: { x: 5, y: 1 },
+        pawn7: { x: 6, y: 1 },
+        pawn8: { x: 7, y: 1 }
       };
 
       constructor() {
-           this.knightPosition$.subscribe(np => {
-                this.currentPosition.knight = np;
+           this.knightPosition1$.subscribe(np => {
+                this.currentPosition.knight1 = np;
            })
-           this.pawnPosition$.subscribe(pp => {
-                this.currentPosition.pawn = pp;
+           this.knightPosition2$.subscribe(np => {
+                this.currentPosition.knight2 = np;
            })
-           this.rookPosition$.subscribe(rp => {
-                this.currentPosition.rook = rp;
+           this.rookPosition1$.subscribe(rp => {
+                this.currentPosition.rook1 = rp;
            })
-           this.bishopPosition$.subscribe(bp => {
-                this.currentPosition.bishop = bp;
+           this.rookPosition2$.subscribe(rp => {
+                this.currentPosition.rook2 = rp;
+           })
+           this.bishopPosition1$.subscribe(bp => {
+                this.currentPosition.bishop1 = bp;
+           })
+           this.bishopPosition2$.subscribe(bp => {
+                this.currentPosition.bishop2 = bp;
            })
            this.queenPosition$.subscribe(qp => {
                 this.currentPosition.queen = qp;
@@ -42,107 +70,163 @@ export class GameService {
            this.kingPosition$.subscribe(kp => {
                 this.currentPosition.king = kp;
            })
+           this.pawnPosition1$.subscribe(pp => {
+                this.currentPosition.pawn1 = pp;
+           })
+           this.pawnPosition2$.subscribe(pp => {
+                this.currentPosition.pawn2 = pp;
+           })
+           this.pawnPosition3$.subscribe(pp => {
+                this.currentPosition.pawn3 = pp;
+           })
+           this.pawnPosition4$.subscribe(pp => {
+                this.currentPosition.pawn4 = pp;
+           })
+           this.pawnPosition5$.subscribe(pp => {
+                this.currentPosition.pawn5 = pp;
+           })
+           this.pawnPosition6$.subscribe(pp => {
+                this.currentPosition.pawn6 = pp;
+           })
+           this.pawnPosition7$.subscribe(pp => {
+                this.currentPosition.pawn7 = pp;
+           })
+           this.pawnPosition8$.subscribe(pp => {
+                this.currentPosition.pawn8 = pp;
+           })
       }
 
         isPieceAt(pos: Coord): boolean {
-              const { knight, pawn, rook, bishop, queen, king } = this.currentPosition;
-              return (knight.x === pos.x && knight.y === pos.y)
-                  || (pawn.x === pos.x && pawn.y === pos.y)
-                  || (rook.x === pos.x && rook.y === pos.y)
-                  || (bishop.x === pos.x && bishop.y === pos.y)
+              const  { rook1, knight1, bishop1 , queen, king, bishop2, knight2, rook2,
+                       pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8} = this.currentPosition;
+              return (knight1.x === pos.x && knight1.y === pos.y)
+                  || (knight2.x === pos.x && knight2.y === pos.y)
+                  || (pawn1.x === pos.x && pawn1.y === pos.y)
+                  || (pawn2.x === pos.x && pawn2.y === pos.y)
+                  || (pawn3.x === pos.x && pawn3.y === pos.y)
+                  || (pawn4.x === pos.x && pawn4.y === pos.y)
+                  || (pawn5.x === pos.x && pawn5.y === pos.y)
+                  || (pawn6.x === pos.x && pawn6.y === pos.y)
+                  || (pawn7.x === pos.x && pawn7.y === pos.y)
+                  || (pawn8.x === pos.x && pawn8.y === pos.y)
+                  || (rook1.x === pos.x && rook1.y === pos.y)
+                  || (rook2.x === pos.x && rook2.y === pos.y)
+                  || (bishop1.x === pos.x && bishop1.y === pos.y)
+                  || (bishop2.x === pos.x && bishop2.y === pos.y)
                   || (queen.x === pos.x && queen.y === pos.y)
                   || (king.x === pos.x && king.y === pos.y);
             }
 
-        getPieceType(pos: Coord) {
-                const { knight, pawn, rook, bishop, queen, king } = this.currentPosition;
-                if (knight.x === pos.x && knight.y === pos.y) {
-                  return "knight";
-                } else if (pawn.x === pos.x && pawn.y === pos.y) {
-                  return "pawn";
-                } else if (rook.x === pos.x && rook.y === pos.y) {
-                  return "rook";
-                } else if (bishop.x === pos.x && bishop.y === pos.y) {
-                  return "bishop";
+        getPieceType(pos: Coord): {piece: string, index: number} {
+                const { rook1, knight1, bishop1 , queen, king, bishop2, knight2, rook2,
+                        pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8} = this.currentPosition;
+                if (knight1.x === pos.x && knight1.y === pos.y) {
+                  return { piece: "knight", index: 1};
+                } else if (knight2.x === pos.x && knight2.y === pos.y) {
+                  return { piece: "knight", index: 2};
+                } else if (rook1.x === pos.x && rook1.y === pos.y) {
+                  return { piece: "rook", index: 1};
+                } else if (rook2.x === pos.x && rook2.y === pos.y) {
+                  return { piece: "rook", index: 2};
+                } else if (bishop1.x === pos.x && bishop1.y === pos.y) {
+                  return { piece: "bishop", index: 1};
+                } else if (bishop2.x === pos.x && bishop2.y === pos.y) {
+                  return { piece: "bishop", index: 2};
                 } else if (queen.x === pos.x && queen.y === pos.y) {
-                  return "queen";
+                  return { piece: "queen", index: 1};
                 } else if (king.x === pos.x && king.y === pos.y) {
-                  return "king";
+                  return { piece: "king", index: 1};
+                } else if (pawn1.x === pos.x && pawn1.y === pos.y) {
+                  return { piece: "pawn", index: 1};
+                } else if (pawn2.x === pos.x && pawn2.y === pos.y) {
+                  return { piece: "pawn", index: 2};
+                } else if (pawn3.x === pos.x && pawn3.y === pos.y) {
+                  return { piece: "pawn", index: 3};
+                } else if (pawn4.x === pos.x && pawn4.y === pos.y) {
+                  return { piece: "pawn", index: 4};
+                } else if (pawn5.x === pos.x && pawn5.y === pos.y) {
+                  return { piece: "pawn", index: 5};
+                } else if (pawn6.x === pos.x && pawn6.y === pos.y) {
+                  return { piece: "pawn", index: 6};
+                } else if (pawn7.x === pos.x && pawn7.y === pos.y) {
+                  return { piece: "pawn", index: 7};
+                } else if (pawn8.x === pos.x && pawn8.y === pos.y) {
+                  return { piece: "pawn", index: 8};
                 } else {
-                  return "";
+                  return { piece: "", index: -1};
                 }
               }
 
-        getValidMoves(pieceType: string, pos: Coord) {
+        getValidMoves(pieceType: {piece: string, index: number}, pos: Coord) {
           const validMoves: Coord[] = [];
 
-          if (pieceType === "knight") {
+          if (pieceType.piece === "knight") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMoveKnight(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMoveKnight(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
               }
             }
-          } else if (pieceType === "pawn") {
+          } else if (pieceType.piece === "pawn") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMovePawn(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMovePawn(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
               }
             }
-          } else if (pieceType === "rook") {
+          } else if (pieceType.piece === "rook") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMoveRook(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMoveRook(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
               }
             }
-          } else if (pieceType === "bishop") {
+          } else if (pieceType.piece === "bishop") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMoveBishop(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMoveBishop(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
               }
             }
-          } else if (pieceType === "queen") {
+          } else if (pieceType.piece === "queen") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMoveQueen(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMoveQueen(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
               }
             }
-          } else if (pieceType === "king") {
+          } else if (pieceType.piece === "king") {
             for (let i = 0; i <= 7; i++) {
               for(let j = 0; j <= 7; j++) {
                 const toX = i;
                 const toY = j;
                 if (!(toX == pos.x && toY == pos.y)) {
-                  if (this.canMoveKing(pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
+                  if (this.canMoveKing(pieceType.index, pos, { x: toX, y: toY }) && !this.isPieceAt({ x: toX, y: toY })) {
                     validMoves.push({ x: toX, y: toY });
                   }
                 }
@@ -155,32 +239,71 @@ export class GameService {
 
 
 
-        moveKnight(from: Coord, to: Coord) {
-          if (this.canMoveKnight(from, to)) {
-            this.knightPosition$.next(to);
+        moveKnight(index: number, from: Coord, to: Coord) {
+          if (this.canMoveKnight(index, from, to)) {
+            (index == 1) ? this.knightPosition1$.next(to) : this.knightPosition2$.next(to);
           }
         }
 
-        movePawn(from: Coord, to: Coord) {
-          if (this.canMovePawn(from, to)) {
-            this.pawnPosition$.next(to);
-          }
-        }
-
-        moveRook(from: Coord, to: Coord) {
-                  if (this.canMoveRook(from, to)) {
-                    this.rookPosition$.next(to);
-                  }
-                }
-
-        moveBishop(from: Coord, to: Coord) {
-              if (this.canMoveBishop(from, to)) {
-                this.bishopPosition$.next(to);
-              }
+        movePawn(index: number, from: Coord, to: Coord) {
+          if (this.canMovePawn(index, from, to)) {
+            switch (index) {
+              case 1:
+                this.pawnPosition1$.next(to);
+                break;
+              case 2:
+                this.pawnPosition2$.next(to);
+                break;
+              case 3:
+                this.pawnPosition3$.next(to);
+                break;
+              case 4:
+                this.pawnPosition4$.next(to);
+                break;
+              case 5:
+                this.pawnPosition5$.next(to);
+                break;
+              case 6:
+                this.pawnPosition6$.next(to);
+                break;
+              case 7:
+                this.pawnPosition7$.next(to);
+                break;
+              case 8:
+                this.pawnPosition8$.next(to);
+                break;
+              default:
+                break;
             }
+          }
+        }
 
-        canMoveKnight(from: Coord, to: Coord) {
-          const { x, y } = this.currentPosition.knight;
+        moveRook(index: number, from: Coord, to: Coord) {
+          if (this.canMoveRook(index, from, to)) {
+            (index == 1) ? this.rookPosition1$.next(to) : this.rookPosition2$.next(to);
+          }
+        }
+
+        moveBishop(index: number, from: Coord, to: Coord) {
+          if (this.canMoveBishop(index, from, to)) {
+            (index == 1) ? this.bishopPosition1$.next(to) : this.bishopPosition2$.next(to);
+          }
+        }
+
+        moveQueen(index: number, from: Coord, to: Coord) {
+          if (this.canMoveQueen(index, from, to)) {
+            (index == 1) ? this.queenPosition$.next(to) : this.queenPosition$.next(to);
+          }
+        }
+
+        moveKing(index: number, from: Coord, to: Coord) {
+          if (this.canMoveKing(index, from, to)) {
+            (index == 1) ? this.kingPosition$.next(to) : this.kingPosition$.next(to);
+          }
+        }
+
+
+        canMoveKnight(index: number, from: Coord, to: Coord) {
           const dx = to.x - from.x;
           const dy = to.y - from.y;
 
@@ -188,20 +311,19 @@ export class GameService {
             (Math.abs(dx) === 1 && Math.abs(dy) === 2);
         }
 
-        canMovePawn(from: Coord, to: Coord) {
-          const { x, y } = this.currentPosition.pawn;
+        canMovePawn(index: number, from: Coord, to: Coord) {
+          const piece = `pawn${index}` as keyof typeof this.currentPosition;
+          const { x, y } = this.currentPosition[piece];
           const dx = to.x - from.x;
           const dy = to.y - from.y;
 
-          return (dx === 0 && dy === 1) ||
-            (dx === 0 && dy === 2 && y === 1) ||
-            (Math.abs(dx) === 1 && dy === 1);
+          return (dx === 0 && dy === 1) || (dx === 0 && dy === 2 && y === 1 && from.y === 1 && to.y === 3) ;
         }
 
-        canMoveRook(from: Coord, to: Coord) {
+
+        canMoveRook(index: number, from: Coord, to: Coord) {
           const { x: fromX, y: fromY } = from;
           const { x: toX, y: toY } = to;
-          const { x: rookX, y: rookY } = this.currentPosition.rook;
 
           if (fromX !== toX && fromY !== toY) {
             return false;
@@ -218,17 +340,14 @@ export class GameService {
           return true;
         }
 
-      canMoveBishop(from: Coord, to: Coord) {
+      canMoveBishop(index: number, from: Coord, to: Coord) {
         const { x: fromX, y: fromY } = from;
         const { x: toX, y: toY } = to;
-        const { x: bishopX, y: bishopY } = this.currentPosition.bishop;
 
-        // Check if the bishop is moving diagonally
         if (Math.abs(toX - fromX) !== Math.abs(toY - fromY)) {
           return false;
         }
 
-        // Check if there are any pieces in the way
         const dx = Math.sign(toX - fromX);
         const dy = Math.sign(toY - fromY);
         for (let x = fromX + dx, y = fromY + dy; x !== toX || y !== toY; x += dx, y += dy) {
@@ -240,13 +359,7 @@ export class GameService {
         return true;
       }
 
-      moveKing(from: Coord, to: Coord) {
-        if (this.canMoveKing(from, to)) {
-          this.kingPosition$.next(to);
-        }
-      }
-
-      canMoveKing(from: Coord, to: Coord) {
+      canMoveKing(index: number, from: Coord, to: Coord) {
         const { x: fromX, y: fromY } = from;
         const { x: toX, y: toY } = to;
         if (Math.abs(fromX - toX) <= 1 && Math.abs(fromY - toY) <= 1) {
@@ -256,14 +369,9 @@ export class GameService {
         return false;
       }
 
-      moveQueen(from: Coord, to: Coord) {
-        if (this.canMoveQueen(from, to)) {
-          this.queenPosition$.next(to);
-        }
-      }
 
-      canMoveQueen(from: Coord, to: Coord) {
-        if (this.canMoveRook(from, to) || this.canMoveBishop(from, to)) {
+      canMoveQueen(index: number, from: Coord, to: Coord) {
+        if (this.canMoveRook(index, from, to) || this.canMoveBishop(index, from, to)) {
           const piece = this.isPieceAt(to);
           return !piece;
         }
