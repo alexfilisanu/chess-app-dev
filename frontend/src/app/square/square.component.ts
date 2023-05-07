6f805a1a58fccfcd94d38322e09d3b0485d06195
alexfilisanu <mihaifilisanu@gmail.com>
2023-05-07 20:05:58 +0300

    coorect move pawn and refactor
    Signed-off-by: alexfilisanu <mihaifilisanu@gmail.com>

--- frontend/src/app/square/square.component.ts
+++ frontend/src/app/square/square.component.ts
@@ -1,27 +1,33 @@
 import { Component, Input } from '@angular/core';
 
+enum BackgroundColor {
+    LightTile = '#61543D',
+    DarkTile = '#BAA378',
+    ValidTile = '#689657',
+    BorderTile = 'solid #61543D 0.5px'
+}
+
 @Component({
-  selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+    selector: 'app-square',
+    templateUrl: './square.component.html',
+    styleUrls: ['./square.component.css']
 })
 export class SquareComponent {
     @Input() black !: boolean;
     @Input() isValid !: boolean;
 
-      getStyle() {
-          return this.black
-              ? { backgroundColor: '#BAA378', color: 'black' }
-              : { backgroundColor: 'rgb(97, 84, 61)', color: 'black' }
-      }
+    getStyle() {
+        return this.black
+            ? { backgroundColor: BackgroundColor.DarkTile }
+            : { backgroundColor: BackgroundColor.LightTile }
+    }
 
-     getValid() {
-       return this.isValid
-         ? {
-             backgroundColor: 'rgb(104, 150, 87)',
-             border: 'solid rgb(97, 84, 61) 0.5px',
-             color: 'black'
-           }
-         : this.getStyle();
-     }
+    getValid() {
+        return this.isValid
+            ? {
+                backgroundColor: BackgroundColor.ValidTile,
+                border: BackgroundColor.BorderTile
+              }
+            : this.getStyle();
+    }
 }
