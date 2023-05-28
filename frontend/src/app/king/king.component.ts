426ef5c64eca5c6f3c2dda5895d9d902d39acac1
alexfilisanu <mihaifilisanu@gmail.com>
2023-05-28 23:52:10 +0300

    added scoala altfel chess pieces

--- frontend/src/app/king/king.component.ts
+++ frontend/src/app/king/king.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-king',
-    template: `<span [style.font-family]="fontFamily">{{ isWhite ? '♔' : '♚' }}</span>`,
-    styles: [`
-        span {
-            font-weight: 400;
-            font-size: 54px;
-            line-height: 70px;
-        }
-    `]
+  selector: 'app-king',
+  template: `<img [src]="isWhite ? whiteKingImagePath : blackKingImagePath" [style.width.px]="imageSize">`,
+  styles: [`
+    img {
+      width: 100%;
+      max-width: 38px;
+    }
+  `]
 })
 export class KingComponent {
-    @Input() isWhite !: boolean;
-    @Input() fontFamily = 'Arial';
+  @Input() isWhite !: boolean;
+  @Input() imageSize = 45;
+  whiteKingImagePath = 'assets/chess-pieces/white-king.png';
+  blackKingImagePath = 'assets/chess-pieces/black-king.png';
 }
--- frontend/src/app/pawn/pawn.component.ts
+++ frontend/src/app/pawn/pawn.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-pawn',
-    template: `<span [style.font-family]="fontFamily">{{ isWhite ? '♙' : '♟' }}</span>`,
-    styles: [`
-        span {
-            font-weight: 400;
-            font-size: 54px;
-            line-height: 70px;
-        }
-    `]
+  selector: 'app-pawn',
+  template: `<img [src]="isWhite ? whitePawnImagePath : blackPawnImagePath" [style.width.px]="imageSize">`,
+  styles: [`
+    img {
+      width: 100%;
+      max-width: 35px;
+    }
+  `]
 })
 export class PawnComponent {
-    @Input() isWhite !: boolean;
-    @Input() fontFamily = 'Arial';
+  @Input() isWhite !: boolean;
+  @Input() imageSize = 45;
+  whitePawnImagePath = 'assets/chess-pieces/white-pawn.png';
+  blackPawnImagePath = 'assets/chess-pieces/black-pawn.png';
 }
